3a71f4ba3f5be6fc452fe9ea0ef9416a8892e273
Peter Zhou <sddlpeter@gmail.com>
2023-03-11 19:08:22 -0800

    add visitor

--- src/com/atguigu/visitor/Client.java
+++ src/com/atguigu/visitor/Client.java
@@ -2,6 +2,21 @@ package com.atguigu.visitor;
 
 public class Client {
     public static void main(String[] args) {
+        ObjectStructure objectStructure = new ObjectStructure();
+        objectStructure.attach(new Man());
+        objectStructure.attach(new Man());
+        objectStructure.attach(new Women());
 
+        // ------- 成功 ---------
+        Action success = new Success();
+        objectStructure.display(success);
+        System.out.println("----------------");
+        // ------- 失败 ---------
+        Action fail = new Fail();
+        objectStructure.display(fail);
+        System.out.println("----------------");
+        // ------- 待定 ---------
+        Action wait = new Wait();
+        objectStructure.display(wait);
     }
 }
