75380dbb7d3b6aea4bde843932d8fdf9db35928a
Hao Zhou <v-zhhao@microsoft.com>
2023-03-20 17:25:44 -0700

    add chain of responsibility

--- src/com/atguigu/responsibilitychain/Client.java
+++ src/com/atguigu/responsibilitychain/Client.java
@@ -2,7 +2,7 @@ package com.atguigu.responsibilitychain;
 
 public class Client {
     public static void main(String[] args) {
-        PurchaseRequest purchaseRequest = new PurchaseRequest(1, 1000f, 1);
+        PurchaseRequest purchaseRequest = new PurchaseRequest(1, 5100f, 1);
         DepartmentApprover departmentApprover = new DepartmentApprover("张主任");
         CollegeApprover collegeApprover = new CollegeApprover("李院长");
         ViceSchoolMasterApprover viceSchoolMasterApprover = new ViceSchoolMasterApprover("王副校长");
@@ -13,7 +13,7 @@ public class Client {
         viceSchoolMasterApprover.setApprover(schoolMasterApprover);
         schoolMasterApprover.setApprover(departmentApprover);
 
-        schoolMasterApprover.processRequest(purchaseRequest);
+        departmentApprover.processRequest(purchaseRequest);
 
     }
 }
